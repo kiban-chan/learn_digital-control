0ac02021ef42d1cddade31914a32ba508d2dde3e
sugihara <kazuya.sugihara@kawadarobot.co.jp>
2019-12-06 14:27:52 +0900

    compare continuous vs discrete

--- first-order-lag.c
+++ first-order-lag.c
@@ -3,10 +3,10 @@
 #include <math.h>
 
 // サンプリング周期
-#define T_s 0.001  // 0.01s
+#define T_s 0.001  // 0.001s
 
 // プラントモデル
-#define T 0.1 // 時定数 0.5s
+#define T 0.1 // 時定数 0.1s
 #define K 1 // DCゲイン 1
 
   /*
@@ -20,6 +20,8 @@ int main(void){
   double u_k1 = 0;
   double y_k0 = 0;
   double y_k1 = 0;
+  double y_c = 0;
+  double err = 0;
 
   FILE *fp_u, *fp_y;
   fp_u = fopen("./step_input1.csv", "r");
@@ -32,10 +34,16 @@ int main(void){
     //printf("file opened\n");
 
     while(fscanf(fp_u, "%lf,%lf", &time, &u_k0) != EOF){
-			//time += time * T_s;
+      // 離散時間系のシミュレーション(双一次変換)
       y_k0 = pow((T_s + 2*T), -1) * (K*T_s*(u_k0 + u_k1) - (T_s - 2*T)*y_k1);
-      //printf("%f\t%f\n", time, u_k0);
-      fprintf(fp_y, "%lf,%lf\n", time, y_k0);
+
+      // 連続時間系のシミュレーション
+      y_c = u_k0 * K * (1 - exp(-(time / T)));
+
+      // (誤差) = (連続) - (双一次)
+      err = y_c - y_k0;
+
+      fprintf(fp_y, "%lf,%lf,%lf,%lf\n", time, y_k0, y_c, err);
 
       u_k1 = u_k0;
       y_k1 = y_k0;
